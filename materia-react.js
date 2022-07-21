56573bd4bce587a4068e2b053600eef8f0b11cba
sdelvalle57 <sdelvalle57@gmail.com>
2022-07-21 10:32:58 -0500

    stringify

--- materia-react.js
+++ materia-react.js
@@ -146,7 +146,7 @@ class Todo extends React.Component {
         try {
             const sig = await getSignature(ethersjs, materiaContract, tokens, address);
             console.log(sig)
-            console.log(tokens.map(t => ethers.BigNumber.from(t)))
+            console.log(JSON.stringify(tokens.map(t => ethers.BigNumber.from(t))))
             let tx = await materiaContract.mint(tokens, sig);
             tx = await tx.wait()
             console.log(tx)
