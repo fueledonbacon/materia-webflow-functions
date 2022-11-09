ed78bb04e1be8852f43ad2c5ffd574582156f697
sdelvalle57 <sdelvalle57@gmail.com>
2022-11-09 09:41:20 -0500

    webpack build

--- src/index.js
+++ src/index.js
@@ -14,3 +14,16 @@ ReactDOM.render(
     React.createElement(App, {}, null),
     document.getElementById('react-target')
 );
+
+
+/*
+<script src="https://unpkg.com/react@15/dist/react.js"></script>
+<script src="https://unpkg.com/react-dom@15/dist/react-dom.js"></script>
+
+<script src="https://cdn.jsdelivr.net/gh/fueledonbacon/materia-webflow-functions@1c22e28/materia-functions.js" type="application/javascript"></script>
+
+<script 
+	type="text/babel" 
+	src="https://cdn.jsdelivr.net/gh/fueledonbacon/materia-webflow-functions@1c22e28/materia-react.js">
+</script>
+*/
